a9fb1d0f58914aa2b455b10b83d7bf4bf7f0ae90
Daniel F <yo.eldany@gmail.com>
2023-05-14 05:42:44 -0300

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-TIMESTAMP=`date +%Y-%m-%d`
-sed -i "s/ENV REFRESHED_AT .*$/ENV REFRESHED_AT $TIMESTAMP/" Dockerfile
-sudo docker build -t diegocortassa/tacticdev .
-sudo docker build -t diegocortassa/tacticdev:4.9 .
-sudo docker build -t diegocortassa/tacticdev:4.9.0.a01 .
-sudo docker push diegocortassa/tacticdev
-sudo docker push diegocortassa/tacticdev:4.9
-sudo docker push diegocortassa/tacticdev:4.9.0.a01
